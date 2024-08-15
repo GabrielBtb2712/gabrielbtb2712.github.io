450c006d170e1014e0def860ac90f8d7ec875853
root <root@localhost.localdomain>
2024-08-15 17:08:57 +0200

    content

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "Command And Control",
+        "excerpt":"Definition and Objective A Command and Control (C&amp;C) is a software system designed to control a client from a server. It allows an operator to execute malware or other commands on remote machines. This document explains the basic implementation of a simple C&amp;C project. Simple C&amp;C Project GitHub Link: Simple...","categories": ["programming"],
+        "tags": ["python","malware","C&C","programming"],
+        "url": "http://localhost:4000/CommandAndControl/",
+        "teaser":"http://localhost:4000/assets/images/Command-And-Control/cAndc.jpg"},{
+        "title": "Presidential - VulnHub",
+        "excerpt":"The Presidential Elections within the USA are just around the corner (November 2020). One of the political parties is concerned that the other political party is going to perform electoral fraud by hacking into the registration system, and falsifying the votes. The state of Ontario has therefore asked you (an...","categories": ["vulnhub","infosec"],
+        "tags": ["hashcat","phpmyadmin","ssh","mysql","capabilities"],
+        "url": "http://localhost:4000/vulnhub-Presidential/",
+        "teaser":"http://localhost:4000/assets/images/2024-08-14-vulnhub-Presidential/web_interface.png"}]
