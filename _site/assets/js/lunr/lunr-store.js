40bea5844fa0a977d125bbd932b9f9716368b4c7
KR4V3N <gabriel2712bocanegrat@gmail.com>
2025-02-23 20:44:04 -0600

    actualizacion

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,4 @@
 var store = [{
-        "title": "Command And Control",
-        "excerpt":"Definition and Objective A Command and Control (C&amp;C) is a software system designed to control a client from a server. It allows an operator to execute malware or other commands on remote machines. This document explains the basic implementation of a simple C&amp;C project. Simple C&amp;C Project GitHub Link: Simple...","categories": ["programming"],
-        "tags": ["python","malware","C&C","programming"],
-        "url": "http://localhost:4000/CommandAndControl/",
-        "teaser":"http://localhost:4000/assets/images/Command-And-Control/cAndc.jpg"},{
-        "title": "Presidential - VulnHub",
-        "excerpt":"The Presidential Elections within the USA are just around the corner (November 2020). One of the political parties is concerned that the other political party is going to perform electoral fraud by hacking into the registration system, and falsifying the votes. The state of Ontario has therefore asked you (an...","categories": ["vulnhub","infosec"],
-        "tags": ["hashcat","phpmyadmin","ssh","mysql","capabilities"],
-        "url": "http://localhost:4000/vulnhub-Presidential/",
-        "teaser":"http://localhost:4000/assets/images/2024-08-14-vulnhub-Presidential/web_interface.png"},{
-        "title": "Infovore - VulnHub",
-        "excerpt":"Hack The Machine This is an easy to intermediate box that shows you how you can exploit innocent looking php functions and lazy sys admins. There are 4 flags in total to be found, and you will have to think outside the box and try alternative ways to achieve your...","categories": ["vulnhub","web vulnerabilities","scripting","containers","cracking"],
-        "tags": ["john the ripper","ssh","phpinfo","abusing","RCE","LFI","docker breakout","pyhthon"],
-        "url": "http://localhost:4000/vulnhub-infovore/",
-        "teaser":"http://localhost:4000/assets/images/2024-08-15-vulnhub-infovore/interface_web.png"},{
         "title": "eCPPTv2 Simulation",
         "excerpt":"This time we will perform a simulation of the eCPPTv2 exam, in which we will have to compromise an entire network of computers using Pivoting, all in a local environment using VMware. Machines links: Aragog Nagini Fawkes Matrix 1 Brainpan We start with the configuration: Network Configuration =&gt; Vmware &gt;...","categories": ["vulnhub","Certification","Pivoting","eCPPTv2"],
         "tags": ["john","WordPress","wpscan","scripting","Vmware","Buffer Over Flow","mysql","SSRF","BurpSuite","Privilege Escalation","Python3"],
