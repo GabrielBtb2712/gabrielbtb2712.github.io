4f75a1dd10c6f692f6f6cba4f04ac1125e63b718
KR4V3N <gabriel2712bocanegrat@gmail.com>
2025-02-23 23:30:38 -0600

    Documentacion 1

---
+++ _posts/2025-02-23-HTB-maquina-Cap.md
@@ -0,0 +1,15 @@
+---
+layout: single
+title:  Maquina Cap HTB
+excerpt: "Documentacion de resolucion de la maquina Cap de dificultad facil de HTB"
+date: 2025-02-23
+classes: wide
+header:
+  teaser: /assets/images/htb-Cap/Cap.png
+  teaser_home_page: true 
+categories:
+  - HackTheBox
+tags:
+  - Privilege Escalation
+  - Python3
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"This time we will perform a simulation of the eCPPTv2 exam, in which we will have to compromise an entire network of computers using Pivoting, all in a local environment using VMware. Machines links: Aragog Nagini Fawkes Matrix 1 Brainpan We start with the configuration: Network Configuration =&gt; Vmware &gt;...","categories": ["vulnhub","Certification","Pivoting","eCPPTv2"],
         "tags": ["john","WordPress","wpscan","scripting","Vmware","Buffer Over Flow","mysql","SSRF","BurpSuite","Privilege Escalation","Python3"],
         "url": "http://localhost:4000/eCPPTv2-simulation/",
-        "teaser":"http://localhost:4000/assets/images/2024-08-17-eCPPTv2-simulation/certificate-eCPPTv2.png"}]
+        "teaser":"http://localhost:4000/assets/images/2024-08-17-eCPPTv2-simulation/certificate-eCPPTv2.png"},{
+        "title": "Maquina Cap HTB",
+        "excerpt":" ","categories": ["HackTheBox"],
+        "tags": ["Privilege Escalation","Python3"],
+        "url": "http://localhost:4000/HTB-maquina-Cap/",
+        "teaser":"http://localhost:4000/assets/images/htb-Cap/Cap.png"}]
